8f97ecaa93bcd2e843878a2d7bfc398b669ec0d7
himanshurajput8 <rajputhimanshu913@gmail.com>
2025-10-12 10:54:25 +0530

    change ui

--- src/destinationList.jsx
+++ src/destinationList.jsx
@@ -57,10 +57,10 @@ export function DestinationList(){
                         <li>5,459 properties</li>
                         <li>England</li>
                         <li>80,602 properties</li>
-                        <li> Zanibar</li>
-                        <li> 882 properties</li>
-                        <li> Phuket Province</li>
-                        <li> 5,547 properties</li>                   
+                        <li>Zanibar</li>
+                        <li>882 properties</li>
+                        <li>Phuket Province</li>
+                        <li>5,547 properties</li>                   
                     </ul>
                 </div>
             </div>
