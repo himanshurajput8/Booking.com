ceae02d594b491d08d6c4bffc92c33c4cd3546d3
himanshurajput8 <rajputhimanshu913@gmail.com>
2025-10-30 07:11:13 +0530

    add functionality

--- src/destinationList.jsx
+++ src/destinationList.jsx
@@ -55,12 +55,12 @@ export function DestinationList(){
                 <ul>
                         <li>Cornwall</li>
                         <li>5,459 properties</li>
-                        <li>England</li>
-                        <li>80,602 properties</li>
-                        <li> Zanibar</li>
-                        <li>882 properties</li>
-                        <li>Phuket Province</li>
-                        <li>5,547 properties</li>                   
+                        <li> England</li>
+                        <li> 80,602 properties</li>
+                        <li>Zanibar</li>
+                        <li> 882 properties</li>
+                        <li> Phuket Province</li>
+                        <li> 5,547 properties</li>                   
                     </ul>
                 </div>
             </div>
