7a1afd54892b94bc89835497f5ed99fce15482cc
himanshurajput8 <rajputhimanshu913@gmail.com>
2025-11-24 23:15:51 +0530

    add new content

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -49,10 +49,10 @@ export function FooterComponent(){
                     <li>About Booking.com</li>
                     <li> How we work</li>
                     <li>Sustainability</li>
-                    <li> Press centre</li>
-                    <li> Careers</li>
-                    <li> Investor relations</li>
-                    <li> Corporate contact</li> 
+                    <li>Press centre</li>
+                    <li>Careers</li>
+                    <li>Investor relations</li>
+                    <li>Corporate contact</li> 
                 </ul>
             </div>
         </div> 
