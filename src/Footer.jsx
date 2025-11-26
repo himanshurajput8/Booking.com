3bc17508520e7d7aef3d7461919396628c398031
himanshurajput8 <rajputhimanshu913@gmail.com>
2025-11-26 07:39:55 +0530

    change icon in header

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -49,8 +49,8 @@ export function FooterComponent(){
                     <li>About Booking.com</li>
                     <li> How we work</li>
                     <li>Sustainability</li>
-                    <li> Press centre</li>
-                    <li> Careers</li>
+                    <li>Press centre</li>
+                    <li>Careers</li>
                     <li> Investor relations</li>
                     <li> Corporate contact</li> 
                 </ul>
