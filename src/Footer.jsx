cddd396526552cd1f56c508d65c8e8224ec56afa
himanshurajput8 <rajputhimanshu913@gmail.com>
2025-12-19 07:56:04 +0530

    add button

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -48,8 +48,8 @@ export function FooterComponent(){
                 <ul>
                     <li>About Booking.com</li>
                     <li> How we work</li>
-                    <li> Sustainability</li>
-                    <li>Press centre</li>
+                    <li>Sustainability</li>
+                    <li> Press centre</li>
                     <li>Careers</li>
                     <li>Investor relations</li>
                     <li>Corporate contact</li> 
